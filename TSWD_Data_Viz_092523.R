dda841e80d377ed1c5862d2a21a455251260edc0
Breanna Niekamp <35186004+breannanpr@users.noreply.github.com>
2023-09-26 08:47:11 -0600

    updated data files

--- TSWD_Data_Viz_092523.R
+++ TSWD_Data_Viz_092523.R
@@ -1,4 +1,13 @@
 #install tidyverse and ggplot within tidy verse
 install.packages("tidyverse")
 install.packages("ggplot2")
+install.packages("readxl")
 
+# read and install cardata into workable dataframe. 
+cardat = read.table("carbitrage.xlsx")
+head(cardat)
+
+
+
+dramdat = read.table("dram_shop.xlsx")
+unhoused = read.table("unhoused.xlsx")
